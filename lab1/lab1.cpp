7bfcc1b52622e47ed1d0fdd6a1dd76f31aa3c00e
Steven Johnson <johnson20201@hotmail.com>
2016-08-24 18:15:41 +0000

    pretty picture

---
+++ lab1/lab1.cpp
@@ -0,0 +1,14 @@
+//Steven Johnson
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+cout <<"   |\\_/| "<< endl;
+cout <<"  / @ @ \\ "<< endl;
+cout <<" ( > - < ) "<< endl;
+cout <<"  '>>X<<' "<< endl;
+cout <<"  /  O  \\ "<< endl;
+
+}
