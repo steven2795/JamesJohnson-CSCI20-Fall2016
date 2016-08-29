d1514c29cf2b7d827df2e9930528c058c8f1480f
Steven Johnson <johnson20201@hotmail.com>
2016-08-29 18:08:34 +0000

    lab4 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,9 @@
+// computer chooses a number between 1 and 20
+//user guesses 5
+//computer says wrong its higher
+//user guesses 15
+//computer says wrong its lower
+//user guesses 10
+//computer says wrong its higher
+//user guesses 13
+//computer says correct, you earned 7 points
---
+++ lab4/lab4.cpp
@@ -0,0 +1,33 @@
+//James Johnson
+//08/29/16
+//mad lib
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string adj1 = "big"; 
+    string adj2 = "red";
+    string adj3 = "happy";
+    string firstName = "big";
+    int age1 = 0;
+    float age2 = .5;
+    char punc = '!';
+    cout << "Please give me an adjective. ";
+    cin >> adj1;
+    cout << "Please give me a second adjective. ";
+    cin >> adj2;
+    cout << "Please give me a First Name. ";
+    cin >> firstName;
+    cout << "Please give me a third adjective. ";
+    cin >> adj3;
+    cout << "Please give me a punctuation to help express this adjective. ";
+    cin >> punc;
+    cout << "Please give me a number. ";
+    cin >> age1;
+    cout << "Please give me a decimal number starting with the decimal. For example: .5 ";
+    cin >> age2;
+    cout << "The " << adj1 << " " << adj2 << " dog jumped over the moon. His name was " << firstName << ". He was very " << adj3 << " that he did it" << punc << " " << firstName << " was only " << age1 << " at the time. Although he'd tell you he was " << age1 << " and " << age2 << " years old.";
+    
+}
