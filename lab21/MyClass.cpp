eddc4713cd85ec597e39f108fc24a80d811efdda
Steven Johnson <johnson20201@hotmail.com>
2016-11-28 18:30:00 +0000

    program4 submission

---
+++ lab21/MyClass.cpp
@@ -0,0 +1,14 @@
+class MyClass {
+ public:
+  /*
+   * Constructor
+   * Initialize our member variable num_ to 0
+   */
+  MyClass();
+  /*
+   * Output our member variable using cout
+   */
+  void Output();
+ private:
+  int num_;
+};
